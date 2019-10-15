db1eb8d557b1c9b213845472777d708a085740c1
Richard Basson <bassonrichard@gmail.com>
2019-10-15 19:48:32 +0200

    EchangeRate Value response

---
+++ ExchangeRate.Api/Models/ExchangeRateValueResp.cs
@@ -0,0 +1,14 @@
+﻿using ExchangeRate.Services.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExchangeRate.Api.Models
+{
+    public class ExchangeRateValueResp
+    {
+        public string CurrencyCode { get; set; }
+        public decimal initialValue { get; set; }
+        public decimal convertedValue { get; set; }
+    }
+}
